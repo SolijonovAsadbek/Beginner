af7c5d67eafef93b2c98e392b6538ddf323de0c5
Asadbek <solijonovasadbek25111999@gmail.com>
2022-06-20 08:31:47 +0500

    couple number

--- boolean/boolean16.py
+++ boolean/boolean16.py
@@ -1,3 +1,6 @@
 a = int(input('a ikki xonali son: '))
-two_room = 9 < a < 99
-print(f'{a} soni ikki xonali sonmi? : {two_room}')
+two_room = 9 < a < 100
+twice = a % 2 == 0
+# formula
+result = two_room and twice
+print(f'{a} soni ikki xonali sonmi? : {result}')
