4ff18889b57d3a87529f0b7cabc564729d68a77a
Asadbek <solijonovasadbek25111999@gmail.com>
2022-06-20 09:43:55 +0500

    boolean24.py

---
+++ boolean/boolean24.py
@@ -0,0 +1,7 @@
+a = int(input('a son: '))
+b = int(input('b son: '))
+c = int(input('c son: '))
+
+d = b ** 2 - 4 * a * c
+natija = d >= 0
+print(f'Kvadrat tenglama haqiqiy ildizga egami? : {natija}')
