ea19e04009ede977b55aeb6bdcdaec143b547408
Asadbek <solijonovasadbek25111999@gmail.com>
2022-06-24 10:07:36 +0500

    case

---
+++ match_case/case2.py
@@ -0,0 +1,15 @@
+baho = int(input('Baho nechi? : '))
+
+match baho:
+    case 1:
+        print('Yomon')
+    case 2:
+        print('Qoniqarsiz')
+    case 3:
+        print('Qoniqarli')
+    case 4:
+        print('Yaxsihi')
+    case 5:
+        print('A`lo')
+    case _:
+        print('Xato')
